8f4590f32f53364f2e8cee2bf7343c676b9a5a40
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-01-29 19:19:04 -0500

    Change class name

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const items = document.querySelectorAll('.faq-item')
+const items = document.querySelectorAll('.accordion-item')
 
 items.forEach((item) => {
   item.addEventListener('click', () => {
-    if (item.classList.contains('active')) {
-      item.classList.remove('active')
+    if (item.classList.contains('accordion-item_opened')) {
+      item.classList.remove('accordion-item_opened')
     } else {
-      item.classList.add('active')
+      item.classList.add('accordion-item_opened')
     }
   })
 })
